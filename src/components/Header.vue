f7e7319e86ab9d4199827531ace5122126df5d0d
nikita.dotsenko <indahazzard@gmail.com>
2020-05-31 23:31:12 +0300

    add base layout for game page

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="flex h-30 w-12 w-full h-12">
+        <div class="flex-12 text-gray-700 text-center w-full h-auto rounded bg-topaz repeating-linear">
+            <p class="flex flex-col justify-center h-full">Who am I?</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+    };
+</script>
+
+<style scoped>
+
+</style>
